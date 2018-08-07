ee5b6d259c1657920f8b7582908a5775e21865a1
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2018-08-07 20:47:12 +0800

    添加md5加密方法

---
+++ app/src/main/java/cn/net/polyglot/util/Util.java
@@ -0,0 +1,25 @@
+package cn.net.polyglot.util;
+
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Util {
+
+  public static String md5(String s) throws NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    digest.update(s.getBytes(Charset.forName("UTF-8")));
+    String hex = new BigInteger(1, digest.digest()).toString(16);
+    // 补齐BigInteger省略的前置0
+    return new String(new char[32 - hex.length()]).replace("\0", "0") + hex;
+  }
+
+  public static String md5(String s, Charset charset) throws NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    digest.update(s.getBytes(charset));
+    String hex = new BigInteger(1, digest.digest()).toString(16);
+    // 补齐BigInteger省略的前置0
+    return new String(new char[32 - hex.length()]).replace("\0", "0") + hex;
+  }
+}
