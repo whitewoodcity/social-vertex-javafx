fe1e6f95642770dd1a3aae0997b3f2d9a6bac7b1
ggx <abcgg8@qq.com>
2018-08-07 22:51:38 +0800

    合并代码

--- app/src/main/java/cn/net/polyglot/common/MessageCell.java
+++ app/src/main/java/cn/net/polyglot/common/MessageCell.java
@@ -3,55 +3,83 @@ package cn.net.polyglot.common;
 import cn.net.polyglot.entity.Message;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
-import javafx.scene.text.TextFlow;
 
 public class MessageCell extends ListCell<Message> {
 
-  @Override
-  protected void updateItem(Message item, boolean empty) {
-    super.updateItem(item, empty);
-    if (empty) {
-      setText(null);
-      setGraphic(null);
-    } else {
-      BorderPane root = new BorderPane();
-      HBox msgLine = new HBox();
-      msgLine.setPrefWidth(getPrefWidth() / 2);
-      msgLine.setPadding(new Insets(5));
-      StackPane sp = new StackPane();
-      sp.setPrefWidth(40);
-      ImageView head = new ImageView();
-      head.setImage(new Image("icons/users.png"));
-      head.setFitHeight(40);
-      head.setFitWidth(40);
-      sp.getChildren().add(head);
-      TextFlow tf;
+    private TextArea ta;
 
-      TextField msg = new TextField(item.getMsg());
-      msg.setEditable(false);
-
-      msg.setFont(new Font(16));
-      msg.setStyle("-fx-text-fill: black;" +
-          "-fx-wrap-text: true;" +
-          "-fx-background-color: transparent;" +
-          "-fx-border-color: transparent;");
-      if (item.isMine()) {
-        msgLine.setAlignment(Pos.CENTER_RIGHT);
-        msgLine.getChildren().addAll(msg, head);
-      } else {
-        msgLine.setAlignment(Pos.CENTER_LEFT);
-        msgLine.getChildren().addAll(head, msg);
-      }
-      root.setCenter(msgLine);
-      setGraphic(root);
+    public MessageCell() {
+        this.ta = new TextArea();
+        ta.setEditable(false);
+        ta.setWrapText(true);
+        ta.getStyleClass().add("copyLabel");
     }
-  }
+
+    @Override
+    protected void updateItem(Message item, boolean empty) {
+        super.updateItem(item, empty);
+        if(empty){
+            setText(null);
+            setGraphic(null);
+        }else {
+            BorderPane root=new BorderPane();
+            HBox msgLine=new HBox();
+            msgLine.prefWidthProperty().bind(widthProperty().divide(2));
+            msgLine.setPadding(new Insets(5));
+            StackPane sp=new StackPane();
+            sp.setPrefWidth(40);
+            ImageView head=new ImageView();
+            head.setImage(new Image("icons/users.png"));
+            head.setFitHeight(40);
+            head.setFitWidth(40);
+            sp.getChildren().add(head);
+
+            Label msg=new Label(item.getMsg());
+            msg.maxWidthProperty().bind(widthProperty().divide(2));
+            msg.setFont(new Font(16));
+            msg.setStyle("-fx-text-fill: black;" +
+                    "-fx-wrap-text: true;" +
+                    "-fx-background-color: transparent;" +
+                    "-fx-border-color: transparent;");
+            msgLine.setOnMouseEntered(event -> {
+                ta.setText(msg.getText());
+                ta.setPrefHeight(msg.getHeight());
+                ta.setPrefWidth(msg.getWidth());
+                if(item.isMine()){
+                    msgLine.getChildren().set(0,ta);
+                }else {
+                    msgLine.getChildren().set(1,ta);
+                }
+            });
+            msgLine.setOnMouseExited(event -> {
+                if(item.isMine()){
+                    msg.setAlignment(Pos.TOP_RIGHT);
+                    msgLine.getChildren().set(0,msg);
+                }else {
+                    msg.setAlignment(Pos.TOP_LEFT);
+                    msgLine.getChildren().set(1,msg);
+                }
+            });
+            if(item.isMine()){
+                msg.setAlignment(Pos.TOP_RIGHT);
+                msgLine.setAlignment(Pos.TOP_RIGHT);
+                msgLine.getChildren().addAll(msg,sp);
+            }else {
+                msg.setAlignment(Pos.TOP_LEFT);
+                msgLine.setAlignment(Pos.TOP_LEFT);
+                msgLine.getChildren().addAll(sp,msg);
+            }
+            root.setCenter(msgLine);
+            setGraphic(root);
+        }
+    }
 }
--- app/src/main/java/cn/net/polyglot/controller/MainController.java
+++ app/src/main/java/cn/net/polyglot/controller/MainController.java
@@ -17,60 +17,56 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
 
 
-  public ImageView msg_icon;
-  public ImageView contact_icon;
-  public ListView<String> lv;
-  public ListView<Message> msgList;
+    public ImageView msg_icon;
+    public ImageView contact_icon;
+    public ListView<String> lv;
+    public ListView<Message> msgList;
 
-  private Image normalMsg = new Image("icons/msg_32.png");
-  private Image enterMsg = new Image("icons/msg_enter_32.png");
-  private Image normalContact = new Image("icons/contact_32.png");
-  private Image enterContact = new Image("icons/contact_enter_32.png");
-  private ObservableList<String> strList = FXCollections.observableArrayList("1", "2", "3", "1", "2", "3", "1", "2", "3"
-      , "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3");
-  private ObservableList<Message> msgs = FXCollections.observableArrayList(new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒dsad", false),
-      new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒dsadafdsfsdfdsfdsfdsfdsffdsfsfsfdsfdsfdsfdsfdsfdsfdsfsfsfsfsdfdsfdsfdsfdsfsdfdsdsfdsfdsfsdfsdfdsfsdfdsfdsfdsfdsfsdfdsfdsfdsfds", false),
-      new Message("的撒旦撒打算的撒dsada", false),
-      new Message("的撒旦撒打算的撒d", true),
-      new Message("的撒旦撒打算的撒dsadas", false),
-      new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒dsa", false),
-      new Message("的撒旦撒打算的撒", true),
-      new Message("的撒旦撒打算的撒", true)
-  );
+    private Image normalMsg=new Image("icons/msg_32.png");
+    private Image enterMsg=new Image("icons/msg_enter_32.png");
+    private Image normalContact=new Image("icons/contact_32.png");
+    private Image enterContact=new Image("icons/contact_enter_32.png");
+    private ObservableList<String> strList= FXCollections.observableArrayList("1","2","3","1","2","3","1","2","3"
+    ,"1","2","3","1","2","3","1","2","3","1","2","3","1","2","3","1","2","3");
+    private ObservableList<Message> msgs= FXCollections.observableArrayList(new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒dsad",false),
+            new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒dsadafdsfsdfdsfdsfdsfdsffdsfsfsfdsfdsfdsfdsfdsfdsfdsfsfsfsfsdfdsfdsfdsfdsfsdfdsdsfdsfdsfsdfsdfdsfsdfdsfdsfdsfdsfsdfdsfdsfdsfds",false),
+            new Message("的撒旦撒打算的撒dsada",false),
+            new Message("的撒旦撒打算的撒d",true),
+            new Message("的撒旦撒打算的撒dsadas",false),
+            new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒dsa",false),
+            new Message("的撒旦撒打算的撒",true),
+            new Message("的撒旦撒打算的撒",true)
+            );
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lv.setCellFactory(param -> new ContactCell());
+        lv.setItems(strList);
+        msgList.setCellFactory(param -> new MessageCell());
+        msgList.setEditable(false);
+        msgList.setItems(msgs);
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    lv.setCellFactory(param -> new ContactCell());
-    lv.setItems(strList);
-    msgList.setCellFactory(param -> new MessageCell());
-    msgList.setEditable(false);
-    msgList.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
-      System.out.println(newValue.intValue());
-      msgList.requestFocus();
-    });
-    msgList.setItems(msgs);
-  }
+    }
 
-  public void handlerMsgMouseEntered(MouseEvent mouseEvent) {
-    msg_icon.setImage(enterMsg);
-  }
+    public void handlerMsgMouseEntered(MouseEvent mouseEvent) {
+        msg_icon.setImage(enterMsg);
+    }
 
-  public void handlerMsgMouseExit(MouseEvent mouseEvent) {
-    msg_icon.setImage(normalMsg);
-  }
+    public void handlerMsgMouseExit(MouseEvent mouseEvent) {
+        msg_icon.setImage(normalMsg);
+    }
 
-  public void handlerContactMouseEntered(MouseEvent mouseEvent) {
-    contact_icon.setImage(enterContact);
-  }
+    public void handlerContactMouseEntered(MouseEvent mouseEvent) {
+        contact_icon.setImage(enterContact);
+    }
 
-  public void handlerContactMouseExit(MouseEvent mouseEvent) {
-    contact_icon.setImage(normalContact);
-  }
+    public void handlerContactMouseExit(MouseEvent mouseEvent) {
+        contact_icon.setImage(normalContact);
+    }
 
 }
