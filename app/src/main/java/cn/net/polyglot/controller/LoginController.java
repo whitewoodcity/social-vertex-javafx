ca88b3692375e9ecc6f56fa025e2296df8a73eae
ggx <abcgg8@qq.com>
2018-08-10 00:48:32 +0800

    登陆注册

--- app/src/main/java/cn/net/polyglot/controller/LoginController.java
+++ app/src/main/java/cn/net/polyglot/controller/LoginController.java
@@ -2,9 +2,14 @@ package cn.net.polyglot.controller;
 
 import cn.net.polyglot.config.Constants;
 import cn.net.polyglot.util.Util;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetClientOptions;
+import io.vertx.core.net.NetSocket;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.codec.BodyCodec;
@@ -23,59 +28,65 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginController {
-  public TextField account;
-  public PasswordField psd;
-  public BorderPane loginView;
+    public TextField account;
+    public PasswordField psd;
+    public BorderPane loginView;
 
-  private Stage stage;
+    private Stage stage;
 
-  public void setStage(Stage stage) {
-    this.stage = stage;
-  }
-
-  public void doRegister(ActionEvent actionEvent) {
-    FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("fxml/register.fxml"));
-    try {
-      Parent parent = loader.load();
-      RegisterController controller = loader.getController();
-      controller.setStage(stage);
-      stage.getScene().setRoot(parent);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
-  }
 
-  public void doLogin(ActionEvent actionEvent) {
-    StackPane root = new StackPane();
-    ImageView head = new ImageView();
-    head.setImage(new Image("icons/users.png"));
-    head.setFitWidth(80);
-    head.setFitHeight(80);
-    root.setAlignment(Pos.CENTER);
-    root.getChildren().add(head);
-    stage.getScene().setRoot(root);
-    String user = account.getText();
-    String password = psd.getText();
-    WebClient client = WebClient.create(Vertx.vertx());
-    client.put(Constants.DEFAULT_HTTP_PORT, Constants.SERVER, "/"+Constants.USER+"/"+Constants.LOGIN)
-        .timeout(30000)
-        .as(BodyCodec.jsonObject())
-        .sendJsonObject(new JsonObject()
-            .put(Constants.ID, user)
-            .put(Constants.PASSWORD, Util.md5(password))
-            .put(Constants.VERSION, Constants.CURRENT_VERSION), ar -> {
-          if (ar.succeeded()) {
-            HttpResponse<JsonObject> response = ar.result();
-            System.out.println(response.body());
-            System.out.println(Thread.currentThread());
-            Platform.runLater(()->{
-              stage.getScene().setRoot(loginView);//返回值在Vert.x的eventloop中获取，最好用platform.runlater做一个转发
-            });
-          } else {
-            stage.getScene().setRoot(loginView);
-          }
+    public void doRegister(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("fxml/register.fxml"));
+        try {
+            Parent parent = loader.load();
+            RegisterController controller = loader.getController();
+            controller.setStage(stage);
+            controller.setLoginView(loginView);
+            stage.getScene().setRoot(parent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doLogin(ActionEvent actionEvent) {
+        StackPane root = new StackPane();
+        ImageView head = new ImageView();
+        head.setImage(new Image("icons/users.png"));
+        head.setFitWidth(80);
+        head.setFitHeight(80);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(head);
+        stage.getScene().setRoot(root);
+        String user = account.getText();
+        String password = psd.getText();
+
+        NetClientOptions options = new NetClientOptions().setConnectTimeout(10000);
+         Vertx.vertx().createNetClient(options).connect(Constants.DEFAULT_TCP_PORT, Constants.SERVER, event -> {
+            if (event.succeeded()) {
+                NetSocket netSocket = event.result();
+                Map<String, Object> map = new HashMap<>();
+                map.put(Constants.TYPE, "user");
+                map.put(Constants.SUBTYPE, "login");
+                map.put(Constants.ID, user);
+                map.put(Constants.PASSWORD, Util.md5(password));
+                map.put(Constants.VERSION, Constants.CURRENT_VERSION);
+                JsonObject jsonObject = new JsonObject(map);
+                netSocket.write(jsonObject.toString()+"\r\n");
+                netSocket.handler(buff->{
+                    System.out.println(new String(buff.getBytes()));
+                });
+
+            } else if (event.failed()) {
+                event.cause().printStackTrace();
+            }
         });
-  }
+
+    }
 }
--- app/src/main/java/cn/net/polyglot/controller/RegisterController.java
+++ app/src/main/java/cn/net/polyglot/controller/RegisterController.java
@@ -1,12 +1,27 @@
 package cn.net.polyglot.controller;
 
+import cn.net.polyglot.config.Constants;
+import cn.net.polyglot.util.Util;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,11 +31,17 @@ public class RegisterController {
 
     public TextField account;
     public PasswordField psd;
-
+    public BorderPane registView;
+    private BorderPane loginView;
+    public Label errorInfo;
     private Stage stage;
 
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+
+    public void setLoginView(BorderPane loginView) {
+        this.loginView = loginView;
     }
 
     public void doBack(MouseEvent mouseEvent) {
@@ -29,7 +50,6 @@ public class RegisterController {
             Parent parent=loader.load();
             LoginController controller=loader.getController();
             controller.setStage(stage);
-//            Scene scene=new Scene(parent,400,300);
             stage.getScene().setRoot(parent);
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,6 +57,45 @@ public class RegisterController {
     }
 
     public void doRegister(ActionEvent actionEvent) {
+        StackPane root = new StackPane();
+        ImageView head = new ImageView();
+        head.setImage(new Image("icons/users.png"));
+        head.setFitWidth(80);
+        head.setFitHeight(80);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(head);
+        stage.getScene().setRoot(root);
+        String user = account.getText();
+        String password = psd.getText();
+        WebClient client = WebClient.create(Vertx.vertx());
+        client.put(Constants.DEFAULT_HTTP_PORT, Constants.SERVER, "/"+Constants.USER+"/"+Constants.REGISTER)
+                .timeout(30000)
+                .as(BodyCodec.jsonObject())
+                .sendJsonObject(new JsonObject()
+                        .put(Constants.ID, user)
+                        .put(Constants.PASSWORD, Util.md5(password))
+                        .put(Constants.VERSION, Constants.CURRENT_VERSION), ar -> {
+                    if (ar.succeeded()) {
+                        HttpResponse<JsonObject> response = ar.result();
+                        System.out.println(response.body());
+                        JsonObject jsonObject=response.body();
+                        if(jsonObject.getBoolean("register",false)){
+                            Platform.runLater(()->{
+                                stage.getScene().setRoot(loginView);
+                            });
+                        }else {
+                            Platform.runLater(()->{
+                                stage.getScene().setRoot(registView);
+                                errorInfo.setText(jsonObject.getString("info",""));
+                            });
+                        }
 
+                    } else {
+                        ar.cause().printStackTrace();
+                        Platform.runLater(()->{
+                            stage.getScene().setRoot(registView);
+                        });
+                    }
+                });
     }
 }
